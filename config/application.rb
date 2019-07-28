7bc40cfba6684c700d01e02666bcd33b781fe125
MasanoriIwakura <hagobutler@yahoo.co.jp>
2019-07-28 16:49:40 +0900

    Update scaffold settings

--- config/application.rb
+++ config/application.rb
@@ -11,6 +11,12 @@ module RailsTodo
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.2
 
+    # Generate settings
+    config.generators do |g|
+      g.javascripts false
+      g.stylesheets false
+    end
+
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
